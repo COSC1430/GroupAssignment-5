0cf8b359403826b6409a154a606f0fe30899c01b
pastafazool <juanedrom@gmail.com>
2017-11-13 10:59:15 -0600

    Update Chicken.java

--- src/Chicken.java
+++ src/Chicken.java
@@ -1,37 +1,46 @@
 import java.util.Random;
 class Chicken extends Actor{
 
-    private char moves[] = {'w','s','a','d'};
-    Chicken(){
-        Random rand = new Random();
-        currRow = rand.nextInt(9);
-        currCol = rand.nextInt(9);
-//Checking if Chicken does not start at center
-        while(currRow==4 && currCol==4){
-            currRow = rand.nextInt(9);
-            currCol = rand.nextInt(9);
+    char moves[] = {'w','s','a','d'};
+    public Chicken(){
+        Random RNG = new Random();
+        
+        currenRow = RNG.nextInt(9);
+        
+        currenColumn = RNG.nextInt(9);
+        
+        while(currenRow==4 && currenColumn==4){
+        	currenRow = RNG.nextInt(9);
+            currenColumn = RNG.nextInt(9);
         }
         data = 'C';
     }
     public int move(){
-        Random rand = new Random();
-        int a = rand.nextInt(4);
+        Random RNG = new Random();
+        
+        int a = RNG.nextInt(4);
+        
         char move = moves[a];
-        while(!borderCheck(currRow,currCol,move)){
-            a = rand.nextInt(4);
+        
+        while(!borderCheck(move,currenRow,currenColumn)){
+        	
+            a = RNG.nextInt(4);
+            
             move = moves[a];
         }
-        if(move=='w'){
-            currRow-=1;
+        //user input to decide direction
+        
+        if(move=='w'){	//UP
+        	currenRow-=1; 
         }
-        if(move=='s'){
-            currRow+=1;
+        if(move=='s'){	//DOWN
+        	currenRow+=1;
         }
-        if(move=='a'){
-            currCol-=1;
+        if(move=='a'){	//LEFT
+        	currenColumn-=1;
         }
-        if(move=='d'){
-            currCol+=1;
+        if(move=='d'){	//RIGHT
+        	currenColumn+=1;
         }
         return 1;
     }
