66e6c4d23bc0e189cfc0014471a1750b40295614
Vyas Ramankulangara <vyas01890@outlook.com>
2017-11-13 08:22:01 -0600

    update 7

--- src/Actor.java
+++ src/Actor.java
@@ -3,8 +3,20 @@ abstract class Actor{
     int currCol;
     char data;
 
-    static boolean borderCheck(int currRow, int currCol, char move) {
-        return (move != 'w' || currRow != 0) && (move != 's' || currRow != 8) && (move != 'a' || currCol != 0) && (move != 'd' || currCol != 8);
+    static boolean borderCheck(int currRow, int currCol, char move){
+        if(move=='w' && currRow==0){
+            return false;
+        }
+        if(move=='s' && currRow==8){
+            return false;
+        }
+        if(move=='a' && currCol==0){
+            return false;
+        }
+        if(move=='d' && currCol==8){
+            return false;
+        }
+        return true;
     }
     abstract int move();
 }
